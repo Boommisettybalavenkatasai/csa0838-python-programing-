de3b0015cfc966c7034405893ee332afa66516cd
Boommisettybalavenkatasai <112765749+Boommisettybalavenkatasai@users.noreply.github.com>
2022-09-15 10:32:09 +0530

    Add files via upload

--- array integer.py
+++ array integer.py
@@ -1,14 +1,20 @@
-def num(a):
-    res=0
-    for i in range(0,len(a)):
-        for j in range(i+1,len(a)):
-            if(a[i]==a[j]):
-                res+=1
-    return res
-b=int(input("Enter length of list:"))
-n=[]
-for i in range(0,b):
-    c=int(input("Enter element:"))
-    n.append(c)
-print("LIST:",n)
-print(num(n))
+def minJumps(arr, l, h):
+    if (h == l):
+        return 0
+    if (arr[l] == 0):
+        return float('inf')
+    min = float('inf')
+    for i in range(l + 1, h + 1):
+        if (i < l + arr[l] + 1):
+            jumps = minJumps(arr, i, h)
+            if (jumps != float('inf') and
+                       jumps + 1 < min):
+                min = jumps + 1 
+    return min
+n=int(input("Enter length of list:"))
+arr=[]
+for i in range(0,n):
+    b=int(input("Enter element:"))
+    arr.append(b)
+print("arr[]=",arr)
+print("Minimum jumps:",minJumps(arr, 0, n-1))
